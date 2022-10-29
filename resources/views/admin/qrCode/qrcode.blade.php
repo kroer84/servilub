0015e513dc2aee63987d954d2c69cb6f4daa604d
Miguel Jiménez Gonzélez <kroer17@hotmail.com>
2022-10-29 10:59:14 -0500

    img qr

--- resources/views/admin/qrCode/qrcode.blade.php
+++ resources/views/admin/qrCode/qrcode.blade.php
@@ -1,2 +1,19 @@
 {{ QrCode::format('svg')->size(300)->color(0,0,0)->generate($dato->identificador, 'img/qrs/'.$dato->identificador.'.svg') }}
-<img src={{ asset('img/qrs/'.$dato->identificador.'.svg') }} alt="">
+
+<div>
+    <div>
+        <center>
+
+            <img src="{{ asset('img/qrs/'.$dato->identificador.'.svg') }}" alt="{{ $dato->identificador }}">
+        </center>
+    </div>
+    <div>
+        <center>
+            <h1>
+                {{ $dato->identificador }}
+            </h1>
+        </center>
+    </div>
+</div>
+
+
